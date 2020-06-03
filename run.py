563d397b674971e945cfabc6a9169a79941e5af8
Wa Thone <wathone98@gmail.com>
2020-06-03 23:29:20 +0800

    Difference State function

--- run.py
+++ run.py
@@ -91,6 +91,44 @@ def download_item(browser, item, save_path, download_blacklist):
     except NoSuchElementException:
         save_shortcut(save_path, item)
 
+# Gets the difference state dictionary from past state to present state of file listings
+def get_difference_state(present_state, past_state):
+    result_dict = {}
+    for course_id, course_dict in present_state.items():
+    
+        # Check if the course has already been saved
+        if course_id not in past_state.keys():
+            result_dict[course_id] = course_dict
+            continue
+        
+        # Check if the section title has already been saved
+        past_course_dict = past_state[course_id]
+
+        for title, items in course_dict.items():
+            if title not in past_course_dict.keys():
+                result_dict[course_id][title] = items
+                continue
+
+            # Check if the file name has already been saved
+            past_items = past_course_dict[title]
+            
+            for item_dict in items:
+                in_previous = False
+                item_name = item_dict['name']
+
+                for past_item_dict in past_items:
+                    if past_item_dict['name'] == item_name:
+                        in_previous = True
+
+                if not in_previous:
+                    if course_id not in result_dict.keys():
+                        result_dict[course_id] = {}
+                    if title not in result_dict[course_id].keys():
+                        result_dict[course_id][title] = []
+                    
+                    result_dict[course_id][title].append(item_dict)
+    return result_dict
+
 # -------------- METHODS ABOVE --------------- #
 
 browser = get_browser()
@@ -200,7 +238,9 @@ browser.quit()
     
 if not is_initial:
     # DIFFERENCE DICTIONARY
-    difference_state = {
+    difference_state = get_difference_state(present_state, past_state)
+    print(difference_state)
+    # difference_state = {
         # "247916": {
         # "Start Here": [
         #     {
@@ -225,7 +265,7 @@ if not is_initial:
         #     }
         # ]
         # }
-    }
+    # }
     for course_id, course_dict in difference_state.items():
         save_path = os.path.join(os.getcwd(), 'Updated_materials', course_links[course_id][0]) + os.path.sep
 
